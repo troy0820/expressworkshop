beef8207353ef897e51944e6bc9159af1d15272a
Troy Connor <troy.connor@yahoo.com>
2015-03-13 14:21:44 -0400

    Added random number and adjusted route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  var number = Math.floor((Math.random()*100)+1);
   res.render('index', { title: 'Vagrant Box',
-  						name: 'Troy\'s Vagrant Box' });
+  			name: 'Troy\'s Vagrant Box', 
+			number:number });
 });
 
 module.exports = router;
