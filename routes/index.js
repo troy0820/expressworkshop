dc1ab71820fedafb7625b05560ecdd6dda4be63d
Troy Connor <troy.connor@yahoo.com>
2015-03-13 00:31:44 -0400

    Added favicon, serve-favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Vagrant Box' });
+  res.render('index', { title: 'Vagrant Box',
+  						name: 'Troy\'s Vagrant Box' });
 });
 
 module.exports = router;
