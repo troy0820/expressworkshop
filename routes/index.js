09faa8b37e15f1a1f319391d22847d521fc8b881
Troy Connor <troy.connor@yahoo.com>
2015-04-10 18:18:44 -0400

    Changed random number to 50

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var number = Math.floor((Math.random()*100)+1);
+  var number = Math.floor((Math.random()*50)+1);
   res.render('index', { title: 'Vagrant Box',
   			name: 'Troy\'s Vagrant Box', 
 			number:number });
